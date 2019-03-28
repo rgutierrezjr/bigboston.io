b761053a8ba08ff20b24b9d15cedb2b4cb5dc1fb
ruben <ruben@bigboston.io>
2019-03-27 21:38:26 -0500

    updated port to environment variable.

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.use(express.static('public'))
 app.use('/css', express.static('/public/css'))
 app.use('/js', express.static('/public/js'))
 app.use('/lib', express.static('/public/lib'))
 
 
-const server = app.listen(8080, function(){
-    var port = server.address().port;
+app.listen(port, () => {
     console.log("Server started at http://localhost:%s", port);
 });
