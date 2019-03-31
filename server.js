492e4f7bcafd5c5c7a7793f8bc38a86adb386f27
ruben <ruben@bigboston.io>
2019-03-30 23:10:45 -0500

    removed unnecessary resources and updated main

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static('public'))
 app.use('/css', express.static('/public/css'))
 app.use('/js', express.static('/public/js'))
 app.use('/lib', express.static('/public/lib'))
+app.use('/img', express.static('/public/img'))
 
 
 app.listen(port, () => {
